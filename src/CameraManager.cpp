69f1ce2ce508f9881c61d4081fe512941d92b750
ru2saig <42857541+ru2saig@users.noreply.github.com>
2022-03-18 20:35:28 +0530

    Custom camera class, and follow a celestial body

---
+++ src/CameraManager.cpp
@@ -0,0 +1,83 @@
+#include <CameraManager.hpp>
+#include <rcamera.h>
+#include <PVector3.hpp>
+#include <CelestialBody.hpp>
+#include <iostream>
+using namespace VMath;
+
+void CameraManager::Update()//Camera *camera)
+{
+  // TODO: untarget, somehere
+  if(target) // follow target
+    {
+      resetCamera();
+      SetCameraMode(this->getCamera(), CAMERA_CUSTOM);
+      
+      float r = target->getRadius();
+      cameraPtr->target = target->getPosition();
+      cameraPtr->position = target->getPosition() + (Vector3) {r*2, r*2, r*2};
+    }
+  else
+    { // free roam camera
+      resetCamera();
+      SetCameraMode(this->getCamera(), CAMERA_FREE);
+    }
+
+  UpdateCamera(cameraPtr);
+}
+
+
+void CameraManager::resetCamera()
+{
+  Vector3 pos = cameraPtr->position;
+  Vector3 target = cameraPtr->target;
+  Vector3 up = cameraPtr->up;
+  delete cameraPtr;
+
+  cameraPtr = new Camera { 0 };
+  cameraPtr->position = pos;
+  cameraPtr->target = target;
+  cameraPtr->up = up;
+  cameraPtr->fovy = 45.0f;                                
+  cameraPtr->projection = CAMERA_PERSPECTIVE;
+}
+
+
+void CameraManager::setTarget(CelestialBody *body)
+{
+  // TODO: interpolate towards this
+  cameraPtr->target = body->getPosition();
+  // TODO: interpolate towards this as well
+  // TODO: Make the zoom a little better, perhaps make it something like the follows:
+  cameraPtr->position = body->getPosition() + (Vector3) {20.0, 20.0, 20.0};
+  this->target = body;
+}
+
+CameraManager& CameraManager::instance()
+{
+  static CameraManager* cm = new CameraManager();
+  return *cm;
+}
+
+void CameraManager::unsetTarget()
+{
+  this->target = nullptr;
+}    
+
+CameraManager::CameraManager() //(Camera *camera)
+  : target { nullptr }
+{
+  cameraPtr = new Camera;
+  cameraPtr->position = (Vector3){ 100.0f, 100.0f, 100.0f }; 
+  cameraPtr->target = (Vector3){ 0.0f, 10.0f, 0.0f };     
+  cameraPtr->up = (Vector3){ 0.0f, 1.0f, 0.0f };          
+  cameraPtr->fovy = 45.0f;                                
+  cameraPtr->projection = CAMERA_PERSPECTIVE;
+  
+  SetCameraMode(*cameraPtr, CAMERA_CUSTOM);
+}
+
+CameraManager::~CameraManager()
+{
+  delete cameraPtr;
+}
---
+++ src/include/CameraManager.hpp
@@ -0,0 +1,33 @@
+/*************************************************************
+ * CameraManager - singleton (for now) class that manages a custom camera tailerd fordward
+                   planetarium
+ * Uses rcamera, from raylib with custom methods and variables
+ * Features -
+ * Methods
+*************************************************************/
+#pragma once
+#include <rcamera.h>
+#include <PVector3.hpp>
+#include <CelestialBody.hpp>
+
+enum class CameraModes {
+  FREE_ROAM = 0, // point and click roam the universe
+  TOUR, // tour mode, follow the path
+};
+
+class CameraManager
+{
+public:
+  static CameraManager& instance();  
+  void Update();
+  Camera getCamera() { return *cameraPtr; }
+  void resetCamera();
+  void setTarget(CelestialBody*); // sets target to follow
+  void unsetTarget(); // sets target to nullptr
+  ~CameraManager();
+  
+private:
+  CameraManager();
+  CelestialBody* target;
+  Camera* cameraPtr;
+};   
